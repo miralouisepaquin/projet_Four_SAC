dc32f684fc9ae183364fa4a7033ffee249174c36
Mira Paquin <mira.louise@hotmail.ca>
2022-05-11 21:01:41 -0400

    V1.0
    Fichier de départ donné par le prof en classe

---
+++ s6_e2s_stoneclass-main/src/MyStone.h
@@ -0,0 +1,59 @@
+/* Copyright (C) 2022 Alain Dube
+ * All rights reserved.
+ *
+ * Projet Domotique
+ * Ecole du Web
+* Cours Systèmes embarqués (c)2022
+ *  
+    @file     MyStone.h
+    @author   Alain Dubé
+    @version  1.2 22/02/02 
+    @description
+      Démonstration comment utiliser l'écran STONE
+
+    platform = ESP32
+    OS = Arduino
+    Langage : C++
+
+    Historique des versions
+        Version    Date       Auteur       Description
+        1.1         21/08/15  Alain       Première version du logiciel
+        1.2         22/02/02  Alain       Ajout des méthodes changePage, setLabel, getVersion
+
+**/
+#ifndef MYSTONE_H
+#define MYSTONE_H
+
+#include <string>
+#include "MySerial.h"
+
+//Structure local utilisés pour garder les informations lues de l'écran
+struct datasRead {
+    int id; //Si 0 alors il n'y a pas de données, <0 Numero commande non traitée, >1 Numero de la commande traité
+    char command[80];
+    char name[80];
+    int type;
+    char line[2048];
+};
+
+class MyStone {
+    private:
+        MySerial *mySerial;
+
+        //Transfert de ces deux méthodes en privées depuis la version 1.2
+        int writeIt(std::string cmdFormat2);
+        int readIt(char *data, int len);
+
+    public:
+        MyStone(int speed, uint32_t config, int rxd, int txd);
+        ~MyStone() { };
+
+        datasRead getValidsDatasIfExists(); 
+        
+        //Nouvelles méthodes ajoutées dans la version 1.2
+        void changePage(const char *pageName = "");
+        void setLabel(const char *labelName, const char *value);
+        void getVersion();
+
+};
+#endif
---
+++ s6_e2s_stoneclass-main/src/main.cpp
@@ -0,0 +1,131 @@
+/* Copyright (C) 2022 Alain Dube
+ * All rights reserved.
+ *
+ * Projet Stone de base
+ * Ecole du Web
+ * Cours Systèmes embarqués (c)2022
+ *
+    @file     main.cpp
+    @author   Alain Dubé
+    @version  1.1 22/08/15
+    @description
+      Démonstration comment utiliser le PORT SERIE pour accèder aux fonctionnalités
+      de l'écran STONE en utilisant la classe MyStone et MySerial
+
+    platform = ESP32
+    OS : Arduino
+    Langage : C++
+
+    Historique des versions
+        Version    Date       Auteur       Description
+        1.1         21/08/15    Alain   Première version du logiciel
+        1.2         22/03/02    Alain   Modification de la classe MyStone
+
+    Fonctionnalités implantées
+        Lecture des evénements envoyés par l'écran
+        Envoyer une commande à l'écran
+          Optenir la version du Firmware de l'écran
+
+ * */
+
+#include <Arduino.h>
+
+#define RXD2 18//16
+#define TXD2 19//17
+#define BAUD_RATE 115200
+
+#include <iostream>
+
+#include "MyButton.h"
+MyButton *myButtonT4 = new MyButton();
+MyButton *myButtonT5 = new MyButton();
+
+#include "MyStone.h"
+MyStone *myStone;
+
+std::string intToHexa(int value){
+  char buffer[10];
+  sprintf(buffer , "0x%4X", value);
+  return (buffer);
+};
+
+std::string charToString(char str, std::string formatStr){
+  char buffer[10];
+  sprintf(buffer , formatStr.c_str(), str);
+  return (buffer);
+};
+
+std::string intToString(int value, std::string formatStr){
+  char buffer[10];
+  sprintf(buffer , formatStr.c_str(), value);
+  return (buffer);
+};
+
+
+
+//Thread qui permet de LOOPER et lire le contenu du port serie
+//avec l'aide du la fonction getValidsDatasIfExists
+void readStoneData() {
+  datasRead rd = myStone->getValidsDatasIfExists();
+  //std::cout << "GData : " << intToHexa(abs(rd.id)) << " " << rd.command << " " << rd.name << " " << rd.type << "\n";
+  switch(rd.id){
+
+      case 0x0002: { //Version
+          std::cout << "GData : " << intToHexa(abs(rd.id)) << " " << rd.command << " " << rd.name << " " << rd.type << "\n";
+          std::string stoneVersion = rd.name;
+          std::cout << "Version : " <<  stoneVersion.c_str() << "\n";
+
+          //std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+          break;
+          }
+      }
+
+  if(rd.id<0) std::cout << "Data received ( id: : " << intToHexa(abs(rd.id)) << "  Command: " << rd.command << " Type: " << rd.type<< ")\n";
+}
+
+
+
+void setup() {
+  
+  Serial.begin(9600);
+
+  Serial.println("Stone serial Txd is on pin: "+String(TXD2));
+  Serial.println("Stone serial Rxd is on pin: "+String(RXD2));
+
+  myStone = new MyStone(115200, SERIAL_8N1, RXD2, TXD2);
+
+
+  myButtonT4->init(T4);
+  myButtonT4->autoSensibilisation(); //Trouve la sensibilité automatiquement
+
+  myButtonT5->init(T5);
+  myButtonT5->autoSensibilisation(); //Trouve la sensibilité automatiquement
+
+  cout << std::string("Début de l'exemple Stone de base pour le ESP32")  << "\n";
+}
+
+void loop() { 
+  
+  readStoneData();
+
+  int buttonActionT4 = myButtonT4->checkMyButton();
+      if(buttonActionT4 > 2)  {  //Si appuyé plus de 0.2 secondes
+            Serial.println("Button T4 pressed");
+            if(myStone) myStone->changePage();
+            }
+
+  int buttonActionT5 = myButtonT5->checkMyButton();
+      if(buttonActionT5 > 2)  {  //Si appuyé plus de 0.2 secondes
+            Serial.println("Button T5 pressed");
+
+            //Dans la version  1.2, nous allons remplacer ces lignes pour utiliser la
+            //méthode getVersion()
+            //char cmdFormat2[99];
+            //strcpy(cmdFormat2, "ST<{\"cmd_code\":\"sys_version\",\"type\":\"system\"}>ET");
+            //std::cout << cmdFormat2 << "\n";
+            //myStone->writeIt((char*)cmdFormat2);
+            if(myStone) myStone->getVersion();
+
+          }
+  }
